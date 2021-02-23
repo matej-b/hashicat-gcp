2998ed746c65ac09b110bcc0aa5db93c093feff7
Matej Brus <matej.brus@gmail.com>
2021-02-23 12:02:23 +0000

    Added vpc module.

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/matej-training/network/google"
+  version = "3.1.1"
+  network_name = "matej-network"
+  project_id = var.project
+  
+  subnets = [
+  {
+    subnet_name   = "matej-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
